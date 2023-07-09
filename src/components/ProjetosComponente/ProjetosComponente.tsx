bc0b54b87230a6682ab8cf2556543fa038b05bc6
jumaelmartins <jumaelmartins@gmail.com>
2023-07-08 23:49:30 -0300

    :recycle: refactor: refatoração componente projetos

--- src/components/ProjetosComponente/ProjetosComponente.tsx
+++ src/components/ProjetosComponente/ProjetosComponente.tsx
@@ -1,72 +1,122 @@
+import React from "react";
 import Filter from "../Icons/Filter";
 import Search from "../Icons/Search";
 import Title from "../Title/Title";
 import Filtro from "./Filtro/Filtro";
 import Form from "./Form/Form";
-import instagramImg from "../../assets/img/instagram-img.png"
+import instagramImg from "../../assets/img/instagram-img.png";
 import Card from "../Card/Cards";
+import JavaScriptIcon from "../Icons/JavaScriptIcon";
 
 const ProjetosComponente = () => {
-
-  const projetos = {
-    projetos: [
-      {
-        title: "projeto 1",
-        id: 0,
-        img: instagramImg,
-        content: "teste"
-      },
-      {
-        title: "projeto 2",
-        id: 1,
-        img: instagramImg,
-        content: "teste"
-      },
-      {
-        title: "projeto 3",
-        id: 2,
-        img: instagramImg,
-        content: "teste"
-      },
-      {
-        title: "projeto 4",
-        id: 3,
-        img: instagramImg,
-        content: "teste"
-      },
-      {
-        title: "projeto 5",
-        id: 4,
-        img: instagramImg,
-        content: "teste"
-      },
-      {
-        title: "projeto 6",
-        id: 5,
-        img: instagramImg,
-        content: "teste"
+  const projetos = [
+    {
+      title: "projeto 1",
+      id: 0,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        html: 1,
+        css: 2
       }
-    ]
-  }
+    },
+    {
+      title: "projeto 2",
+      id: 1,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        sass: 1,
+        css: 2
+      }
+    },
+    {
+      title: "projeto 3",
+      id: 2,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        typescript: 1,
+        css: 2
+      }
+    },
+    {
+      title: "projeto 4",
+      id: 3,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        html: 1,
+        css: 2
+      }
+    },
+    {
+      title: "projeto 5",
+      id: 4,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        html: 1,
+        css: 2
+      }
+    },
+    {
+      title: "projeto 6",
+      id: 5,
+      img: instagramImg,
+      content: "teste",
+      langs: {
+        javascript: 123,
+        html: 1,
+        css: 2
+      }
+    },
+  ];
 
+  const novoArray = projetos.reduce((acc: any, obj) => {
+   
+    const langs = Object.keys(obj.langs);
+    langs.forEach(lang => {
+      if (!acc.includes(lang)) {
+        acc.push(lang);
+      }
+    });
+    return acc;
+  }, [])
+
+const languagesIcon = [
+  {javascript: <JavaScriptIcon/>}
+]
 
   return (
-    <section className="projetos">
-      <div className="protetos-titulo">
+    <section id="projects">
+      <div className="projects--title">
         <Title content="PROJETOS" />
       </div>
-      <div className="filtro">
-        <Filtro icon={<Filter/>} content={["html","javascript", "css"]}/>
+      <div className="filter">
+        <Filtro icon={<Filter />} content={novoArray} />
       </div>
-      <Form placeholder="Busque, por nome, linguagem ou tecnologias" id="search" icon={<Search/>} />
-      <ul className="projetos-card">
-        {
-          projetos.projetos.map(projeto => (
-            <li>
-              <Card variant="third" title={projeto.title} thumb={projeto.img} content={projeto.content} />
-            </li>
-          ))
-        }
+      <Form
+        placeholder="Busque, por nome, linguagem ou tecnologias"
+        id="search"
+        icon={<Search />}
+      />
+      <ul className="projects-card">
+        {projetos.map((projeto) => (
+          <li key={projeto.id}>
+            <Card
+              variant="third"
+              title={projeto.title}
+              thumb={projeto.img}
+              content={projeto.content}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
